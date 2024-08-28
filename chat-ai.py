b97872346751066432b1910d530261da2ce2c5c7
Givanne.nyc <46247851+gbr3zzy@users.noreply.github.com>
2024-08-27 21:58:21 -0400

    Update chat-ai.py

--- chat-ai.py
+++ chat-ai.py
@@ -1,134 +1,119 @@
+import os 
 import streamlit as st
-import os
+import openai
 from PyPDF2 import PdfReader
-import docx
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
 from dotenv import load_dotenv
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from streamlit_chat import message
-from langchain.callbacks import get_openai_callback
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
 
+def get_single_pdf_chunks(pdf, text_splitter):
+    pdf_reader = PdfReader(pdf)
+    pdf_chunks = []
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        page_chunks = text_splitter.split_text(page_text)
+        pdf_chunks.extend(page_chunks)
+    return pdf_chunks
 
+def get_all_pdfs_chunks(pdf_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2500,
+        chunk_overlap=500
+    )
+
+    all_chunks = []
+    for pdf in pdf_docs:
+        pdf_chunks = get_single_pdf_chunks(pdf, text_splitter)
+        all_chunks.extend(pdf_chunks)
+    return all_chunks
+
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings()  # Using OpenAI Embeddings instead of Google
+    try:
+        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+        return vectorstore
+    except Exception as e:
+        st.warning("Issue with reading the PDF/s. Your File might be scanned so there will be nothing in chunks for embeddings to work on")
+
+def get_response(context, question, model_engine="gpt-4"):
+    messages = [
+        {"role": "system", "content": "You are a helpful and informative assistant that answers questions using text from the reference context."},
+        {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}"}
+    ]
+
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_engine,
+            messages=messages,
+            max_tokens=8000,
+            temperature=0.2,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        return response['choices'][0]['message']['content']
+
+    except Exception as e:
+        st.warning(e)
+
+def working_process():
+
+    vectorstore = st.session_state['vectorstore']
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+        AIMessage(content="Hello! I'm a PDF Assistant. Ask me anything about your PDFs or Documents")
+    ]
+    
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.markdown(message.content)
+
+    user_query = st.chat_input("Enter Your Query....")
+    if user_query is not None and user_query.strip() != "":
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+        with st.chat_message("Human"):
+            st.markdown(user_query)
+        
+        with st.chat_message("AI"):
+            try:
+                relevant_content = vectorstore.similarity_search(user_query, k=10)
+                result = get_response(relevant_content, user_query)
+                st.markdown(result)
+                st.session_state.chat_history.append(AIMessage(content=result))
+            except Exception as e:
+                st.warning(e)
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Incidents AI Demo")
-    st.header("Incidents AI Demo")
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-    if "processComplete" not in st.session_state:
-        st.session_state.processComplete = None
+    load_dotenv()
+
+    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
+    st.header("Chat with Multiple PDFs :books:")
+
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    if "vectorstore" not in st.session_state:
+        st.session_state.vectorstore = None
 
     with st.sidebar:
-        uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        process = st.button("Process")
-    if process:
-        if not openai_api_key:
-            st.info("Please add your OpenAI API key to continue.")
-            st.stop()
-        files_text = get_files_text(uploaded_files)
-        text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
-     
-        st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
+        st.subheader("Your Documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and Click on 'Submit'", accept_multiple_files=True)
+        if st.button("Submit"):
+            with st.spinner("Processing"):
+                text_chunks = get_all_pdfs_chunks(pdf_docs)
+                vectorstore = get_vector_store(text_chunks)
+                st.session_state.vectorstore = vectorstore
 
-        st.session_state.processComplete = True
+    if st.session_state.vectorstore is not None:        
+        working_process()
 
-    if  st.session_state.processComplete == True:
-        user_question = st.chat_input("Chat with your file")
-        if user_question:
-            handel_userinput(user_question)
-
-
-
-
-
-def get_files_text(uploaded_files):
-    text = ""
-    for uploaded_file in uploaded_files:
-        split_tup = os.path.splitext(uploaded_file.name)
-        file_extension = split_tup[1]
-        if file_extension == ".pdf":
-            text += get_pdf_text(uploaded_file)
-        elif file_extension == ".docx":
-            text += get_docx_text(uploaded_file)
-        else:
-            text += get_csv_text(uploaded_file)
-    return text
-
-
-def get_pdf_text(pdf):
-    pdf_reader = PdfReader(pdf)
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    return text
-
-def get_docx_text(file):
-    doc = docx.Document(file)
-    allText = []
-    for docpara in doc.paragraphs:
-        allText.append(docpara.text)
-    text = ' '.join(allText)
-    return text
-
-def get_csv_text(file):
-    return "a"
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=900,
-        chunk_overlap=100,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings()
-    knowledge_base = FAISS.from_texts(text_chunks,embeddings)
-    return knowledge_base
-
-def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vetorestore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-# This function takes a user question as input, sends it to a conversation model and displays the conversation history along with some additional information.
-def handel_userinput(user_question):
-
-    with get_openai_callback() as cb:
-        response = st.session_state.conversation({'question':user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    response_container = st.container()
-
-    with response_container:
-        for i, messages in enumerate(st.session_state.chat_history):
-            if i % 2 == 0:
-                message(messages.content, is_user=True, key=str(i))
-            else:
-                message(messages.content, key=str(i))
-        st.write(f"Total Tokens: {cb.total_tokens}" f", Prompt Tokens: {cb.prompt_tokens}" f", Completion Tokens: {cb.completion_tokens}" f", Total Cost (USD): ${cb.total_cost}")
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
