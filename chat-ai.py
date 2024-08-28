b7671e7155fdd140f21071a0c4fdcf174f670ec6
Givanne.nyc <46247851+gbr3zzy@users.noreply.github.com>
2024-08-27 23:15:20 -0400

    Update chat-ai.py

--- chat-ai.py
+++ chat-ai.py
@@ -122,7 +122,7 @@ def main():
 
     load_dotenv()
 
-    st.set_page_config(page_title="Incidents AI", page_icon=":books:")
+    st.set_page_config(page_title="Incidents AI", page_icon=":robot:")
     st.header("Incidents AI :books:")
 
     client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
